6c9d7d0769f85060a011969ceea91a3ad55c760e
Polychronis Patapis <chronispatapis@gmail.com>
2023-12-19 15:18:13 +0100

    refactored to python package

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # mrs-hci
+A package for PSF subtraction and spectral extraction with the JWST/MIRI MRS instrument.
 
 # The code has to be run from the exec.py file. At the moment the paths of the references are hardcoded. This will change hopefully soon.
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='mrshci',
+    version='0.0.1',
+    packages=['mrshci'],
+    url='https://github.com/gcugno/mrs-hci',
+    license='MIT',
+    author='Gabriele Cugno',
+    author_email='gcugno@umich.edu',
+    description='Toolkit for analysis of jwst exoplanet data',
+    install_requires=["jwst >= 1.12.4",
+                      "astropy",
+                      "pynpoint",
+                      "spectres",
+                      "matplotlib"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
